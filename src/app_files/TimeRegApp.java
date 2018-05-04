b372e072cd4175f09e9dc5a3fafd8e56342658da
Rizwan-Ishaq <rizwan@ishaq.dk>
2018-05-04 17:00:47 +0200

    Removed unnecessary function

--- src/app_files/TimeRegApp.java
+++ src/app_files/TimeRegApp.java
@@ -15,12 +15,12 @@ public class TimeRegApp {
 		return userLoggedIn;
 	}
 
-	public void registerResource(Resource resource) throws Exception {
-		if (resources.contains(resource)) {
-			throw new Exception("User is already registered");
-		}
-		resources.add(resource);		
-	}
+//	public void registerResource(Resource resource) throws Exception {
+//		if (resources.contains(resource)) {
+//			throw new Exception("User is already registered");
+//		}
+//		resources.add(resource);		
+//	}
 	
 	public void addWorkPlanned(WorkPlanned WorkPlanned) {
 		workplanned.add(WorkPlanned);
