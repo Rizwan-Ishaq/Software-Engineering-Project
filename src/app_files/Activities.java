dcfbbd6c1886a9a5d9c7de856fd36f6070e4c65a
mohaiman <mohrah2009@gmail.com>
2018-05-04 11:58:56 +0200

    Created class for activities

---
+++ src/app_files/Activities.java
@@ -0,0 +1,40 @@
+package app_files;
+
+public class Activities {
+	
+	private int totalTime;
+	private int startTime;
+	private int endTime;
+	
+	public void Activity(int totalTime, int startTime, int endTime) {
+		this.totalTime = totalTime;
+		this.startTime = startTime;
+		this.endTime = endTime;
+	}
+
+	public int getTotalTime() {
+		return totalTime;
+	}
+
+	public void setTotalTime(int totalTime) {
+		this.totalTime = totalTime;
+	}
+
+	public int getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(int startTime) {
+		this.startTime = startTime;
+	}
+
+	public int getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(int endTime) {
+		this.endTime = endTime;
+	}
+	
+	
+}
