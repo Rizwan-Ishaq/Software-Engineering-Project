adb8751ee4b7fdeddab2e337c0519ca494b6653c
Kayaba Akihiko <Kayaba Akihiko@DESKTOP-UUU0276>
2018-05-05 13:16:08 +0200

    added the feature to check if resource is available or not when some planned work is added, meaning when a manager or whomever creates some scheduled work it'll check if the resource is already assigned some planned work in the time frame.

--- src/app_files/TimeRegApp.java
+++ src/app_files/TimeRegApp.java
@@ -7,11 +7,12 @@ import java.util.List;
 public class TimeRegApp {
 
 	private boolean userLoggedIn = false;
-	private List<Resource> resources = new ArrayList<>();
+	private List<Resource> resourcelist = new ArrayList<>();
 	public List<Project> projectlist = new ArrayList<>();
 	private List<WorkPlanned> workplannedlist = new ArrayList<>();
 	private List<Activity> activitylist = new ArrayList<>();
 
+	// testpurposes
 	Boolean standAloneTest = true;
 
 	public boolean userLoggedIn() {
@@ -45,18 +46,38 @@ public class TimeRegApp {
 
 			throw new IllegalArgumentException("Project doesn't exist.");
 		}
-		
-		if (getActivity(WorkPlanned.getActivityId()).equals(null)) {
+
+		if (getActivity(WorkPlanned.getProjectId(), WorkPlanned.getActivityId()).equals(null)) {
 
 			throw new IllegalArgumentException("Activity doesn't exist.");
 		}
-		
+
 		if (getResource(WorkPlanned.getResourceId()).equals(null)) {
 
 			throw new IllegalArgumentException("Employee doesn't exist.");
 		}
 
-		workplannedlist.add(WorkPlanned);
+		// Check if resource is booked for the time being
+		// If end time is before start time, and it's true that start time is after end time of comparable then resource is free.
+		// written as NOT
+		Boolean resourceAvailable = true;
+
+		for (int i = 0; i < workplannedlist.size(); i++) {
+			if (WorkPlanned.getResourceId() == workplannedlist.get(i).getResourceId()) {
+				if (!(WorkPlanned.getEndWeek() < workplannedlist.get(i).getStartWeek())
+						&& !(WorkPlanned.getStartWeek() > workplannedlist.get(i).getEndWeek())) {
+
+					resourceAvailable = false;
+
+				}
+
+			}
+		}
+
+		if (resourceAvailable) {
+			workplannedlist.add(WorkPlanned);
+		}
+
 	}
 
 	// StefanAndersen
@@ -89,21 +110,56 @@ public class TimeRegApp {
 					return projectlist.get(i);
 				}
 			}
-			
+
 		}
 		return null;
 
 	}
-	
-	public Activity getActivity(String activityId) {
-		
-		for ( int i = 0; i < activitylist.size(); i++) {
-			if (activitylist.get(i).getActId().equals(activityId)) {
-				return activitylist.get(i);
+
+	public Activity getActivity(String projectId, String activityId) {
+
+		if (standAloneTest = true) {
+
+			if (activityId.substring(0, 1) == "A") {
+				return null;
+			} else {
+				Activity activity = new Activity("Testesttest", "Tsettsettsettset", 100, 3, 5);
+				return activity;
+			}
+
+		} else {
+
+			for (int i = 0; i < activitylist.size(); i++) {
+				if (activitylist.get(i).getActId().equals(activityId)
+						&& activitylist.get(i).getProjId().equals(projectId)) {
+					return activitylist.get(i);
+				}
+			}
+			return null;
+		}
+	}
+
+	public Resource getResource(String resourceId) {
+
+		if (standAloneTest = true) {
+
+			if (resourceId.substring(0, 1) == "A") {
+				return null;
+			} else {
+				Resource resource = new Resource("StefanAndersen", "SSA", "hubert");
+				return resource;
+			}
+
+		} else {
+
+			for (int i = 0; i < resourcelist.size(); i++) {
+				if (resourcelist.get(i).getId().equals(resourceId)) {
+					return resourcelist.get(i);
+				}
 			}
 		}
 		return null;
-		
+
 	}
 
 	// StefanAndersen
