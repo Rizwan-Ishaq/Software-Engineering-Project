033ff5afdd0aac96024e7ed18a445ac438254cb9
mohaiman <mohrah2009@gmail.com>
2018-05-06 22:13:51 +0200

    Created registration constructers

---
+++ src/app_files/SpecialActivityRegistration.java
@@ -0,0 +1,11 @@
+//Author: Mohaiman Rahim, S174120
+package app_files;
+
+public class SpecialActivityRegistration {
+	
+	TimeRegApp timeRegApp = new TimeRegApp();
+	
+	public SpecialActivityRegistration(String Resource, String startWeek, String endWeek, String startDay, String endDay) {
+		
+	}
+}
---
+++ src/app_files/TimeRegistration.java
@@ -0,0 +1,11 @@
+//Author: Mohaiman Rahim, S174120
+package app_files;
+
+public class TimeRegistration {
+	
+	TimeRegApp timeRegApp = new TimeRegApp();
+	
+	public TimeRegistration(String Resource, String Week, String Day, int hoursWorked) {
+		
+	}
+}
